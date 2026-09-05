7da3f77fbc704ad903e5fd6fe96a4be24b13e91b
BeanoTW <beanotarren@gmail.com>
2026-09-05 11:42:19 +0100

    Register player registration schema v19

--- src/lib/game/migrations/index.ts
+++ src/lib/game/migrations/index.ts
@@ -15,6 +15,7 @@ import { SCOUTING_MIGRATIONS } from "./v14-v15";
 import { CLUB_IDENTITY_MIGRATIONS } from "./v15-v16";
 import { CLUB_REFERENCE_MIGRATIONS } from "./v16-v17";
 import { EMPLOYMENT_MIGRATIONS } from "./v17-v18";
+import { PLAYER_REGISTRATION_MIGRATIONS } from "./v18-v19";
 import type { GameState } from "../types";
 
 export * from "./types";
@@ -28,6 +29,7 @@ export const MIGRATIONS: Migration[] = [
   ...CLUB_IDENTITY_MIGRATIONS,
   ...CLUB_REFERENCE_MIGRATIONS,
   ...EMPLOYMENT_MIGRATIONS,
+  ...PLAYER_REGISTRATION_MIGRATIONS,
 ];
 
 /** Highest version any registered step can produce. */
